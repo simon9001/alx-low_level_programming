864203e25bcad28b09610818a93090d70438abae
Simon9001 <simongatungo300@gmail.com>
2023-02-23 05:46:08 -0800

    0x03-debugging

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _putchar -writes the character c to stdout
+ * @c: the character to print
+ * return: on success 1.
+ * on error.-1 is returned. and error is set approprietly
+ * */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+}
