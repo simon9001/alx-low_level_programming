2ef88233074765b551022f9fe5b34a6fdd5eaf75
Simon9001 <simongatungo300@gmail.com>
2023-02-27 09:29:27 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
-#ifdef main_h
-#define main_h
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,4 +12,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
 #endif
